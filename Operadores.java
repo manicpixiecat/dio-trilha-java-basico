9ee3a3630b6e14f1600d2cc6c9fae7fa690bf7a6
manicpixiecat <tatyibnida@gmail.com>
2025-04-01 20:38:17 +0000

    adicionado exemplos e comentários de operadores relacionais e lógicos

--- Operadores.java
+++ Operadores.java
@@ -9,13 +9,13 @@ public class Operadores {
         concatenacao = 1 + 1 + 1 + "1";
         System.out.println(concatenacao);
 
-        concatenacao = 1 + "1" + 1 + 1;  
+        concatenacao = 1 + "1" + 1 + 1;
         System.out.println(concatenacao);
 
-        concatenacao = 1 + "1" + 1 + "1";  
+        concatenacao = 1 + "1" + 1 + "1";
         System.out.println(concatenacao);
 
-        concatenacao = "1" + 1 + 1 + 1; 
+        concatenacao = "1" + 1 + 1 + 1;
         System.out.println(concatenacao);
 
         concatenacao = "1" + (1 + 1 + 1);
@@ -48,5 +48,48 @@ public class Operadores {
         // Operador lógico de negação usado para inverter o valor booleano
         boolean verdadeiro = true;
         System.out.println("Inverteu" + !verdadeiro);
+
+        //operadores relacionais > < ==  !=  <= >= etc
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 > numero2)
+            System.out.print("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+            System.out.print("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+            System.out.print("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+            System.out.print("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+            System.out.print("Numero 1 é diferente de numero 2");
+
+        //exemplos de operadores ternários
+        int a,b;
+        a = 5;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+
+        boolean condicao1=true;
+        boolean condicao2=true;
+        
+        // verifica se as condições juntas são verdadeiras
+        if (condicao1 && condicao2) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+        
+        //Verifica se PELO MENOS UMA condição é verdadeira com o simbolo || ( Verifica se PELO MENOS UMA condição é verdadeira (OR)
+        if (condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira");
+        }
+        
+        System.out.println("Fim");
     }
 }
